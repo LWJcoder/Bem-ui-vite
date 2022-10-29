72273ae4a778fffbe81e2ed0ab773ceb0d529a50
Johnny <15818464582@163.com>
2022-10-29 18:10:52 +0800

    📝docs(注释): n

--- src/dom.js
+++ src/dom.js
@@ -4,3 +4,20 @@ exports.generateDiv = () => {
   div.className = 'c1'
   document.body.appendChild(div)
 }
+
+// 数组铺平
+// function flat1(arr) {
+//   if (Array.isArray(arr)) {
+//     arr.forEach((item) => {
+//       if (Array.isArray(item)) {
+//         item = flat1(item)
+//       }
+//     })
+//     return arr.join(',')
+//   }
+// }
+
+// function flat2(arr) {
+//   let str = flat1(arr)
+//   return str.split(',')
+// }
